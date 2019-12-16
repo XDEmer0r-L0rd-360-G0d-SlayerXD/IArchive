b03fe92fc502c6679a41b88e5b01626644379bde
XDEmer0r-L0rd-360-G0d-SlayerXD <namisboss5@gmail.com>
2019-12-16 02:22:58 -0600

    Got checkbox to appear when clicking another one

--- Archive_Base.py
+++ Archive_Base.py
@@ -11,19 +11,35 @@ class SetupContainer:
                 user_box.config(fg='black')
 
         self.root = Tk()
+        self.grab_posts_frame = Frame(self.root)
+        self.grab_smiles_frame = Frame(self.root)
         self.var = IntVar()
+        self.want_posts = IntVar()
+        self.want_repubs = IntVar()
         user_box = Entry(self.root)
         user_box.insert(0, 'Enter Username')
         user_box.bind('<FocusIn>', on_entry_click)
-        user_box.pack()
+        post_check = Checkbutton(self.grab_posts_frame, text='Grab posts', variable=self.want_posts, command=self.post_options)
         c = Checkbutton(self.root, text="Enable Tab", variable=self.var, command=self.cb)
-        c.pack()
         but = Button(self.root, text='Start (double check options before)', command=self.done)
+        self.repub_check = Checkbutton(self.grab_posts_frame, text='Exclude Repubs', variable=self.want_repubs)
+        user_box.pack()
+        self.grab_posts_frame.pack()
+        post_check.pack()
+        self.grab_smiles_frame.pack()
+        c.pack()
         but.pack()
+
         self.root.mainloop()
 
     def cb(self, event=None):
         print("variable is", self.var.get())
+
+    def post_options(self):
+        if self.want_posts.get() == 1:
+            self.repub_check.pack()
+        else:
+            self.repub_check.forget()
 
     def done(self):
         self.root.quit()
