818a220cce35d90ec2d0dc59c2b20a5d37291666
XDEmer0r-L0rd-360-G0d-SlayerXD <namisboss5@gmail.com>
2019-12-16 01:59:34 -0600

    Made text box where pretext disapears when clicked

--- Archive_Base.py
+++ Archive_Base.py
@@ -3,8 +3,19 @@ from tkinter import *
 
 class SetupContainer:
     def __init__(self):
+        def on_entry_click(event):
+            """function that gets called whenever user_box is clicked"""
+            if user_box.get() == 'Enter Username':
+                user_box.delete(0, "end")  # delete all the text in the user_box
+                user_box.insert(0, '')  # Insert blank for user input
+                user_box.config(fg='black')
+
         self.root = Tk()
         self.var = IntVar()
+        user_box = Entry(self.root)
+        user_box.insert(0, 'Enter Username')
+        user_box.bind('<FocusIn>', on_entry_click)
+        user_box.pack()
         c = Checkbutton(self.root, text="Enable Tab", variable=self.var, command=self.cb)
         c.pack()
         but = Button(self.root, text='Start (double check options before)', command=self.done)
