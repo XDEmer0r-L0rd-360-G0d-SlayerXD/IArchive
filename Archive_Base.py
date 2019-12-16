efba8c679b0871383184c8b97646e635245866e8
XDEmer0r-L0rd-360-G0d-SlayerXD <namisboss5@gmail.com>
2019-12-16 02:52:28 -0600

    Prepared Auth code for getting smile posts

--- Archive_Base.py
+++ Archive_Base.py
@@ -1,14 +1,17 @@
 from tkinter import *
+import requests
+import os
+import selenium
 
 
 class SetupContainer:
     def __init__(self):
         def on_entry_click(event):
-            """function that gets called whenever user_box is clicked"""
-            if user_box.get() == 'Enter Username':
-                user_box.delete(0, "end")  # delete all the text in the user_box
-                user_box.insert(0, '')  # Insert blank for user input
-                user_box.config(fg='black')
+            """function that gets called whenever self.user_box is clicked"""
+            if self.user_box.get() == 'Enter Username':
+                self.user_box.delete(0, "end")  # delete all the text in the self.user_box
+                self.user_box.insert(0, '')  # Insert blank for user input
+                self.user_box.config(fg='black')
 
         self.root = Tk()
         self.grab_posts_frame = Frame(self.root)
@@ -16,17 +19,23 @@ class SetupContainer:
         self.var = IntVar()
         self.want_posts = IntVar()
         self.want_repubs = IntVar()
-        user_box = Entry(self.root)
-        user_box.insert(0, 'Enter Username')
-        user_box.bind('<FocusIn>', on_entry_click)
+        self.want_smiled = IntVar()
+        self.user_box = Entry(self.root)
+        self.user_box.insert(0, 'Enter Username')
+        self.user_box.bind('<FocusIn>', on_entry_click)
         post_check = Checkbutton(self.grab_posts_frame, text='Grab posts', variable=self.want_posts, command=self.post_options)
-        c = Checkbutton(self.root, text="Enable Tab", variable=self.var, command=self.cb)
         but = Button(self.root, text='Start (double check options before)', command=self.done)
         self.repub_check = Checkbutton(self.grab_posts_frame, text='Exclude Repubs', variable=self.want_repubs)
-        user_box.pack()
+        smile_check = Checkbutton(self.grab_smiles_frame, text='Grab Smiled Posts', variable=self.want_smiled, command=self.check_auth_frame)
+        self.auth_label = Label(self.grab_smiles_frame, text='Authentication Unconfirmed')
+        self.auth_button = Button(self.grab_smiles_frame, text='Test Authentication', command=self.auth_check)
+        # c is for example, will be removed
+        c = Checkbutton(self.root, text="Enable Tab", variable=self.var, command=self.cb)
+        self.user_box.pack()
         self.grab_posts_frame.pack()
         post_check.pack()
         self.grab_smiles_frame.pack()
+        smile_check.pack()
         c.pack()
         but.pack()
 
@@ -41,7 +50,25 @@ class SetupContainer:
         else:
             self.repub_check.forget()
 
+    def check_auth_frame(self):
+        if self.want_smiled.get() == 1:
+            self.auth_label.pack()
+            self.auth_button.pack()
+        else:
+            self.auth_label.forget()
+            self.auth_button.forget()
+
+    def auth_check(self):
+        try:
+            with open(f'{self.user_box}_key.txt') as f:
+                login_cookie = f.read()
+        except FileNotFoundError:
+
+            self.auth_label.config(text='checked')
+
     def done(self):
+        # function gets called twice, by button and by box.done() outside
+        print('want', self.want_repubs.get())
         self.root.quit()
         return 2
 
