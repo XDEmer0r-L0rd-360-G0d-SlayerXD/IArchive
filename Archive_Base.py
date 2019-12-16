e357c8410194b5d34cdf7039015c37cfe5d921bf
XDEmer0r-L0rd-360-G0d-SlayerXD <namisboss5@gmail.com>
2019-12-16 00:12:49 -0600

    Created a tkinter box to start with; it has a checkbox and button.

--- Archive_Base.py
+++ Archive_Base.py
@@ -1,5 +1,33 @@
+from tkinter import *
+
+
+class SetupContainer:
+    def __init__(self):
+        self.root = Tk()
+        self.var = IntVar()
+        c = Checkbutton(self.root, text="Enable Tab", variable=self.var, command=self.cb)
+        c.pack()
+        but = Button(self.root, text='Start (double check options before)', command=self.done)
+        but.pack()
+        self.root.mainloop()
+
+    def cb(self, event=None):
+        print("variable is", self.var.get())
+
+    def done(self):
+        self.root.quit()
+        return 2
+
+
+def pre_setup():
+    box = SetupContainer()
+    values = box.done()
+    print('val:', values)
+    return 0
+
 
 def main():
+    values = pre_setup()
     print('teast')
 
 
