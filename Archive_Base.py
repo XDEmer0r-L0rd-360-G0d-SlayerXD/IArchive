e7849bd2d5a5808b624d9999dd3eabb4cdad322f
XDEmer0r-L0rd-360-G0d-SlayerXD <namisboss5@gmail.com>
2019-12-16 17:15:06 -0600

    Updated .gitignore, added geckodriver.exe and implemented selenium to get UID cookie after login

--- Archive_Base.py
+++ Archive_Base.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import requests
 import os
-import selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 class SetupContainer:
@@ -31,14 +32,11 @@ class SetupContainer:
         smile_check = Checkbutton(self.grab_smiles_frame, text='Grab Smiled Posts', variable=self.want_smiled, command=self.check_auth_frame)
         self.auth_label = Label(self.grab_smiles_frame, text='Authentication Unconfirmed')
         self.auth_button = Button(self.grab_smiles_frame, text='Test Authentication', command=self.auth_check)
-        # c is for example, will be removed
-        c = Checkbutton(self.root, text="Enable Tab", variable=self.var, command=self.cb)
         self.user_box.pack()
         self.grab_posts_frame.pack()
         post_check.pack()
         self.grab_smiles_frame.pack()
         smile_check.pack()
-        c.pack()
         but.pack()
 
         self.root.mainloop()
@@ -64,27 +62,60 @@ class SetupContainer:
         self.auth_button.forget()
         try:
             print('user:', self.user_box.get())
-            with open(f'{self.user_box.get()}_key.txt') as f:
+            with open(f'{self.user_box.get()}_key.txt', 'r') as f:
                 self.login_cookie = f.read()
             self.auth_label.config(text='Key Found')
             print(self.login_cookie)
         except FileNotFoundError:
             def get_enter_val():
                 pass_value = pass_box.get()
+                email_value = email_box.get()
+                print('cred', email_value, pass_value)
                 self.auth_label.config(text=pass_value)
+                self.login_cookie = self.get_cookies(email_value, pass_value)
 
-            def on_entry_click(event):
+            def on_entry_click_pass(event):
                 if pass_box.get() == 'Password needed':
                     pass_box.delete(0, "end")  # delete all the text in the self.user_box
                     pass_box.insert(0, '')  # Insert blank for user input
                     pass_box.config(fg='black')
-            pass_value = ''
+
+            def on_entry_click_email(event):
+                if email_box.get() == 'Email needed':
+                    email_box.delete(0, "end")  # delete all the text in the self.user_box
+                    email_box.insert(0, '')  # Insert blank for user input
+                    email_box.config(fg='black')
+
+            self.auth_label.config(text='No key found')
             pass_box = Entry(self.grab_smiles_frame)
             pass_box.insert(0, 'Password needed')
-            pass_box.bind('<FocusIn>', on_entry_click)
-            pass_button = Button(self.grab_smiles_frame, text='Enter Password', command=get_enter_val)
+            pass_box.bind('<FocusIn>', on_entry_click_pass)
+            email_box = Entry(self.grab_smiles_frame)
+            email_box.insert(0, 'Email needed')
+            email_box.bind('<FocusIn>', on_entry_click_email)
+            pass_button = Button(self.grab_smiles_frame, text='Get Credentials', command=get_enter_val)
+            email_box.pack()
             pass_box.pack()
             pass_button.pack()
+
+    def get_cookies(self, email, password):
+        # options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox()
+        driver.get('https://ifunny.co')
+        target = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/header/div[3]/ul[2]/li[2]/a')
+        target.click()
+        target = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/section/form/div[1]/div/input')
+        target.send_keys(email)
+        target = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/section/form/div[2]/div/input')
+        target.send_keys(password)
+        target = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/section/form/button[1]')
+        target.click()
+        raw_cookie = driver.get_cookie('UID')
+        driver.quit()
+        print('raw', raw_cookie)
+        with open(f'{self.user_box.get()}_key.txt', 'w') as f:
+            f.write(raw_cookie['value'])
+        return raw_cookie['value']
 
     def done(self):
         # function gets called twice, by button and by box.done() outside
